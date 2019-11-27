5a88389ba32087a1fa4f3b6e7130bba3c93358bb
Jens Kutilek <webmail@kutilek.de>
2019-11-27 16:09:22 +0100

    Remove excess whitespace

--- ShowAllCoordinates.glyphsReporter/Contents/Resources/ShowAllCoordinates.py
+++ ShowAllCoordinates.glyphsReporter/Contents/Resources/ShowAllCoordinates.py
@@ -15,34 +15,35 @@ if not path in sys.path:
 
 import GlyphsApp
 
-GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
+GlyphsReporterProtocol = objc.protocolNamed("GlyphsReporter")
 
-#def angle(p1, p2):
-#   return atan2(p2.y - p1.y, p2.x - p1.x)
+# def angle(p1, p2):
+#    return atan2(p2.y - p1.y, p2.x - p1.x)
 
-class ShowAllCoordinates ( NSObject, GlyphsReporterProtocol ):
-    
-    def init( self ):
+
+class ShowAllCoordinates (NSObject, GlyphsReporterProtocol):
+
+    def init(self):
         """
         Put any initializations you want to make here.
         """
         try:
-            #Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
+            # Bundle = NSBundle.bundleForClass_(NSClassFromString(self.className()))
             return self
         except Exception as e:
-            self.logToConsole( "init: %s" % str(e) )
-    
-    def interfaceVersion( self ):
+            self.logToConsole("init: %s" % str(e))
+
+    def interfaceVersion(self):
         """
-        Distinguishes the API version the plugin was built for. 
+        Distinguishes the API version the plugin was built for.
         Return 1.
         """
         try:
             return 1
         except Exception as e:
-            self.logToConsole( "interfaceVersion: %s" % str(e) )
-    
-    def title( self ):
+            self.logToConsole("interfaceVersion: %s" % str(e))
+
+    def title(self):
         """
         This is the name as it appears in the menu in combination with 'Show'.
         E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
@@ -50,9 +51,9 @@ class ShowAllCoordinates ( NSObject, GlyphsReporterProtocol ):
         try:
             return "All Coordinates"
         except Exception as e:
-            self.logToConsole( "title: %s" % str(e) )
-    
-    def keyEquivalent( self ):
+            self.logToConsole("title: %s" % str(e))
+
+    def keyEquivalent(self):
         """
         The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
         Pretty tricky to find a shortcut that is not taken yet, so be careful.
@@ -61,9 +62,9 @@ class ShowAllCoordinates ( NSObject, GlyphsReporterProtocol ):
         try:
             return None
         except Exception as e:
-            self.logToConsole( "keyEquivalent: %s" % str(e) )
-    
-    def modifierMask( self ):
+            self.logToConsole("keyEquivalent: %s" % str(e))
+
+    def modifierMask(self):
         """
         Use any combination of these to determine the modifier keys for your default shortcut:
             return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
@@ -74,72 +75,73 @@ class ShowAllCoordinates ( NSObject, GlyphsReporterProtocol ):
         try:
             return 0
         except Exception as e:
-            self.logToConsole( "modifierMask: %s" % str(e) )
-    
-    def drawForegroundForLayer_( self, Layer ):
+            self.logToConsole("modifierMask: %s" % str(e))
+
+    def drawForegroundForLayer_(self, Layer):
         try:
             for path in Layer.paths:
                 for segment in path.segments:
                     for pt in segment:
-                        #phi = degrees(angle(prev_pt, pt))
-                        #if phi > 270 or -90 < phi < 90:
-                        #   textAlignment = 5
-                        #   # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
-                        #else:
-                        #   textAlignment = 3
-                        self.drawTextAtPoint( " %g, %g" % (pt.x, pt.y), (pt.x, pt.y), 6 )
-                        #prev_pt = pt
+                        # phi = degrees(angle(prev_pt, pt))
+                        # if phi > 270 or -90 < phi < 90:
+                        #    textAlignment = 5
+                        #    # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
+                        # else:
+                        #    textAlignment = 3
+                        self.drawTextAtPoint(" %g, %g" % (pt.x, pt.y), (pt.x, pt.y), 6)
+                        # prev_pt = pt
             for anchor in Layer.anchors:
-                self.drawTextAtPoint(" %s, %s" % (anchor.position.x, anchor.position.y), (anchor.position.x, anchor.position.y), 6 )
+                self.drawTextAtPoint(" %s, %s" % (anchor.position.x, anchor.position.y), (anchor.position.x, anchor.position.y), 6)
         except Exception as e:
-            self.logToConsole( "drawForegroundForLayer_: %s" % str(e) )
-    
-    def drawBackgroundForLayer_( self, Layer ):
+            self.logToConsole("drawForegroundForLayer_: %s" % str(e))
+
+    def drawBackgroundForLayer_(self, Layer):
         """
         Whatever you draw here will be displayed BEHIND the paths.
         """
         try:
             pass
         except Exception as e:
-            self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
-    
-    def drawBackgroundForInactiveLayer_( self, Layer ):
+            self.logToConsole("drawBackgroundForLayer_: %s" % str(e))
+
+    def drawBackgroundForInactiveLayer_(self, Layer):
         pass
-    
-    def drawTextAtPoint( self, text, textPosition, textAlignment = 3, fontSize=9.0, fontColor=NSColor.brownColor() ):
+
+    def drawTextAtPoint(self, text, textPosition, textAlignment = 3, fontSize=9.0, fontColor=NSColor.brownColor()):
         """
-        Use self.drawTextAtPoint( "blabla", myNSPoint ) to display left-aligned text at myNSPoint.
+        Use self.drawTextAtPoint("blabla", myNSPoint) to display left-aligned text at myNSPoint.
         """
         try:
             glyphEditView = self.controller.graphicView()
             currentZoom = self.getScale()
-            fontAttributes = { 
-                NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
-                NSForegroundColorAttributeName: fontColor }
-            displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
-            glyphEditView.drawText_atPoint_alignment_( displayText, textPosition, textAlignment )
+            fontAttributes = {
+                NSFontAttributeName: NSFont.labelFontOfSize_(fontSize/currentZoom),
+                NSForegroundColorAttributeName: fontColor
+            }
+            displayText = NSAttributedString.alloc().initWithString_attributes_(text, fontAttributes)
+            glyphEditView.drawText_atPoint_alignment_(displayText, textPosition, textAlignment)
         except Exception as e:
-            self.logToConsole( "drawTextAtPoint: %s" % str(e) )
-    
-    def getHandleSize( self ):
+            self.logToConsole("drawTextAtPoint: %s" % str(e))
+
+    def getHandleSize(self):
         """
         Returns the current handle size as set in user preferences.
         Use: self.getHandleSize() / self.getScale()
         to determine the right size for drawing on the canvas.
         """
         try:
-            Selected = NSUserDefaults.standardUserDefaults().integerForKey_( "GSHandleSize" )
+            Selected = NSUserDefaults.standardUserDefaults().integerForKey_("GSHandleSize")
             if Selected == 0:
                 return 5.0
             elif Selected == 2:
                 return 10.0
             else:
-                return 7.0 # Regular
+                return 7.0  # Regular
         except Exception as e:
-            self.logToConsole( "getHandleSize: HandleSize defaulting to 7.0. %s" % str(e) )
+            self.logToConsole("getHandleSize: HandleSize defaulting to 7.0. %s" % str(e))
             return 7.0
 
-    def getScale( self ):
+    def getScale(self):
         """
         self.getScale() returns the current scale factor of the Edit View UI.
         Divide any scalable size by this value in order to keep the same apparent pixel size.
@@ -147,22 +149,22 @@ class ShowAllCoordinates ( NSObject, GlyphsReporterProtocol ):
         try:
             return self.controller.graphicView().scale()
         except:
-            self.logToConsole( "Scale defaulting to 1.0" )
+            self.logToConsole("Scale defaulting to 1.0")
             return 1.0
-    
-    def setController_( self, Controller ):
+
+    def setController_(self, Controller):
         """
         Use self.controller as object for the current view controller.
         """
         try:
             self.controller = Controller
         except Exception as e:
-            self.logToConsole( "Could not set controller" )
-    
-    def logToConsole( self, message ):
+            self.logToConsole("Could not set controller")
+
+    def logToConsole(self, message):
         """
         The variable 'message' will be passed to Console.app.
-        Use self.logToConsole( "bla bla" ) for debugging.
+        Use self.logToConsole("bla bla") for debugging.
         """
-        myLog = "Show %s plugin:\n%s" % ( self.title(), message )
-        NSLog( myLog )
+        myLog = "Show %s plugin:\n%s" % (self.title(), message)
+        NSLog(myLog)
