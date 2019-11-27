092008c7ca7ec632633b1a0a5f04f4ea083b4a4b
Jens Kutilek <webmail@kutilek.de>
2019-11-27 16:25:59 +0100

    Add code to new plugin template

--- ShowAllCoordinates.glyphsReporter/Contents/Resources/plugin.py
+++ ShowAllCoordinates.glyphsReporter/Contents/Resources/plugin.py
@@ -7,60 +7,98 @@ from AppKit import NSAttributedString, NSBundle, NSColor, NSFont, \
     NSFontAttributeName, NSForegroundColorAttributeName, NSObject, \
     NSUserDefaults
 
+# def angle(p1, p2):
+#    return atan2(p2.y - p1.y, p2.x - p1.x)
+
 
 class ShowAllCoordinates(ReporterPlugin):
 
     def settings(self):
-        self.menuName = Glyphs.localize({'en': u'My Plugin', 'de': u'Mein Plugin'})
-        self.generalContextMenus = [
-            {'name': Glyphs.localize({'en': u'Do something', 'de': u'Tu etwas'}), 'action': self.doSomething},
-        ]
+        self.menuName = Glyphs.localize({
+            'en': u'All Coordinates',
+            'de': u'Alle Koordinaten'
+        })
 
     def foreground(self, layer):
-        NSColor.blueColor().set()
-        NSBezierPath.fillRect_(layer.bounds)
-        self.drawTextAtPoint(layer.parent.name, NSPoint(0, 0))
+        for path in layer.paths:
+            for segment in path.segments:
+                for pt in segment:
+                    # phi = degrees(angle(prev_pt, pt))
+                    # if phi > 270 or -90 < phi < 90:
+                    #    textAlignment = 5
+                    #    # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
+                    # else:
+                    #    textAlignment = 3
+                    self.drawTextAtPoint(
+                        " %g, %g" % (pt.x, pt.y),
+                        (pt.x, pt.y),
+                        6
+                    )
+                    # prev_pt = pt
+        for anchor in layer.anchors:
+            self.drawTextAtPoint(
+                " %s, %s" % (anchor.position.x, anchor.position.y),
+                (anchor.position.x, anchor.position.y),
+                6
+            )
 
-    def inactiveLayer(self, layer):
-        NSColor.redColor().set()
-        if layer.paths:
-            layer.bezierPath.fill()
-        if layer.components:
-            for component in layer.components:
-                component.bezierPath.fill()
+    # def inactiveLayer(self, layer):
+    #     return
 
-    def preview(self, layer):
-        NSColor.blueColor().set()
-        if layer.paths:
-            layer.bezierPath.fill()
-        if layer.components:
-            for component in layer.components:
-                component.bezierPath.fill()
-
-    def doSomething(self):
-        print 'Just did something'
-
-    def conditionalContextMenus(self):
-
-        # Empty list of context menu items
-        contextMenus = []
-
-        # Execute only if layers are actually selected
-        if Glyphs.font.selectedLayers:
-            layer = Glyphs.font.selectedLayers[0]
-
-            # Exactly one object is selected and it’s an anchor
-            if len(layer.selection) == 1 and type(layer.selection[0]) == GSAnchor:
-
-                # Add context menu item
-                contextMenus.append({'name': Glyphs.localize({'en': u'Do something else', 'de': u'Tu etwas anderes'}), 'action': self.doSomethingElse})
-
-        # Return list of context menu items
-        return contextMenus
-
-    def doSomethingElse(self):
-        print 'Just did something else'
+    # def preview(self, layer):
+    #     return
 
     def __file__(self):
         """Please leave this method unchanged"""
         return __file__
+
+    def drawTextAtPoint(self, text, textPosition, textAlignment=3, fontSize=9.0, fontColor=NSColor.brownColor()):
+        """
+        Use self.drawTextAtPoint("blabla", myNSPoint) to display left-aligned text at myNSPoint.
+        """
+        try:
+            glyphEditView = self.controller.graphicView()
+            currentZoom = self.getScale()
+            fontAttributes = {
+                NSFontAttributeName: NSFont.labelFontOfSize_(fontSize / currentZoom),
+                NSForegroundColorAttributeName: fontColor
+            }
+            displayText = NSAttributedString.alloc().initWithString_attributes_(text, fontAttributes)
+            glyphEditView.drawText_atPoint_alignment_(
+                displayText,
+                textPosition,
+                textAlignment
+            )
+        except Exception as e:
+            self.logToConsole("drawTextAtPoint: %s" % str(e))
+
+    def getHandleSize(self):
+        """
+        Returns the current handle size as set in user preferences.
+        Use: self.getHandleSize() / self.getScale()
+        to determine the right size for drawing on the canvas.
+        """
+        try:
+            Selected = NSUserDefaults.standardUserDefaults().integerForKey_("GSHandleSize")
+            if Selected == 0:
+                return 5.0
+            elif Selected == 2:
+                return 10.0
+            else:
+                return 7.0  # Regular
+        except Exception as e:
+            self.logToConsole(
+                "getHandleSize: HandleSize defaulting to 7.0. %s" % str(e)
+            )
+            return 7.0
+
+    def getScale(self):
+        """
+        self.getScale() returns the current scale factor of the Edit View UI.
+        Divide any scalable size by this value in order to keep the same apparent pixel size.
+        """
+        try:
+            return self.controller.graphicView().scale()
+        except:
+            self.logToConsole("Scale defaulting to 1.0")
+            return 1.0
